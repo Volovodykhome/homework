264802f4637f705a38742063ebb96c4ad90e2181
Volovodykhome <nastuavololovoduk@gmail.com>
2020-09-23 11:50:50 +0300

    23.09.20line_3

--- src/line3/lesson_1.java
+++ src/line3/lesson_1.java
@@ -7,9 +7,9 @@ public class lesson_1 {
     public static void main(String[] args) {
         System.out.println("Введите конечное число 546");
         int a = t.nextInt();
-        int a1 = a /100;
-        int a2 = ;
+        int a1 = a / 100;
+        int a2 = a / 10 % 10;
         int a3 = a % 10;
-        System.out.println(a1 * 100 + a3 * 10 + a2);
+        System.out.println(a2 * 100 + a1 * 10 + a3);
     }
 }
